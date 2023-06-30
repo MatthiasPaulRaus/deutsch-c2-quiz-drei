1c63dc0a968ff8c1451bda0c33e16b3b4f8aa8f9
matthias paul raus <81903294+MatthiasPaulRaus@users.noreply.github.com>
2023-06-30 17:46:25 +0200

    initial commit

---
+++ deutsch-c2-quiz-drei/src/QuestionsList.jsx
@@ -0,0 +1,221 @@
+const QuestionsList = [
+    {
+        question:"Der behandelnde Arzt untersucht den Patienten.",
+        answersList: [
+            {answer:"Der Patient wird vom behandelnden Arzt untersucht.", isCorrect:true},
+            {answer:"Der Patient war vom behandelnden Arzt untersucht worden.", isCorrect:false},
+            {answer:"Der Patient wäre vom behandelnden Arzt untersucht worden.", isCorrect:false},
+        ]
+    },{
+        question:"Die Tropenkrankheit wird durch Insektenstiche übertragen.",
+        answersList: [
+            {answer:"Durch Stiche der Insekten kann sie nicht übertragen werden.", isCorrect:false},
+            {answer:"Bestimmte Insekten übertragen die Krankheit durch Stiche.", isCorrect:true},
+            {answer:"Die Ansteckung wird möglicherweise durch Insekten erfolgen. ", isCorrect:false},
+        ]
+    },{
+        question:"In der Öffentlichkeit wurde viel über das Gesetz diskutiert.",
+        answersList: [
+            {answer:"Es wäre wohl nie darüber diskutiert worden.", isCorrect:false},
+            {answer:"Es wird bestimmt eine öffentliche Diskussion darüber geben.", isCorrect:false},
+            {answer:"Es wurde viel in der Gesellschaft darüber diskutiert.", isCorrect:true},
+        ]
+    },{
+        question:"Hier wird oft bis spät in die Nacht gearbeitet.",
+        answersList: [
+            {answer:"Man arbeitet hier oft bis kurz vor Mitternacht.", isCorrect:true},
+            {answer:"Nach Mitternacht arbeitet man hier immer.", isCorrect:false},
+            {answer:"Bevor es Nacht wird arbeitet man hier sehr viel.", isCorrect:false},
+        ]
+    },{
+        question:"Das neue Medikament hat mir sofort geholfen.",
+        answersList: [
+            {answer:"Ein neues Medikament hätte mir helfen können.", isCorrect:false},
+            {answer:"Meine Krankheit wurde durch ein neues Medikament geheilt.", isCorrect:true},
+            {answer:"Mit einem neuen Medikament hätte ich geheilt werden können.", isCorrect:false},
+        ]
+    },{
+        question:"Der Hausarzt will den Patienten zu einem Spezialisten überweisen.",
+        answersList: [
+            {answer:"Der Spezialist will den Patienten nicht selbst behandeln.", isCorrect:false},
+            {answer:"Der Patient fordert den Hausarzt auf ihn zum Spezialisten zu schicken.", isCorrect:false},
+            {answer:"Ein Spezialist soll den Patienten behandeln.", isCorrect:true},
+        ]
+    },{
+        question:"Die Grünflächen in der Stadt sollen erweitert werden.",
+        answersList: [
+            {answer:"Man will die Grünflächen in der Stadt vergrößern.", isCorrect:true},
+            {answer:"Man hat die Absicht die Stadt zu begrünen.", isCorrect:false},
+            {answer:"Es wurde beschlossen, den Stadtpark zu verschönern.", isCorrect:false},
+        ]
+    },{
+        question:"Der Bahnhofsvorplatz muss komlett umgebaut werden.",
+        answersList: [
+            {answer:"Man rechnet mit dem schnellen Ende der Bauarbeiten.", isCorrect:false},
+            {answer:"Die Bauarbeiten sind absolut notwendig.", isCorrect:true},
+            {answer:"Der Platz vor dem Bahnhof könnte umgestaltet werden.", isCorrect:false},
+        ]
+    },{
+        question:"Die Maßnahmen hätten längst ergriffen werden sollen.",
+        answersList: [
+            {answer:"Der Schaden kann später noch behoben werden.", isCorrect:false},
+            {answer:"Früher hätten wir uns nicht darum gekümmert.", isCorrect:false},
+            {answer:"Nach allem Hin und Her ist es jetzt zu spät.", isCorrect:true},
+        ]
+    },{
+        question:"Vom Aussterben bedrohte Tierarten",
+        answersList: [
+            {answer:"sollten länderübergreifend geschützt werden.", isCorrect:true},
+            {answer:"könnten in Zukunft eine Gefahr darstellen.", isCorrect:false},
+            {answer:"finden überall einen geeigneten Lebensraum.", isCorrect:false},
+        ]
+    },
+    
+    {
+        question:"Die Umweltprobleme können nur zusammen gelöst werden.",
+        answersList: [
+            {answer:"Ja, das ist nur gemeinsam lösbar.", isCorrect:true},
+            {answer:"Nur, wenn wir alle mitmachen ist das löslich.", isCorrect:false},
+            {answer:"Auch wenn wir es alleine versuchen, ist es möglich.", isCorrect:false},
+        ]
+    },{
+        question:"Die Umweltbelastung ist nicht zu unterschätzen.",
+        answersList: [
+            {answer:"Ja, man kann das genaue Ausmaß nicht wahrnehmen.", isCorrect:false},
+            {answer:"Die Auswirkungen auf die Umwelt sind erheblich.", isCorrect:true},
+            {answer:"An die Konsequenzen braucht man nicht zu denken.", isCorrect:false},
+        ]
+    },{
+        question:"Die Wasserversorgung ist ein schwer zu lösendes Problem.",
+        answersList: [
+            {answer:"Die technischen Probleme sind größtenteils geringfügig.", isCorrect:false},
+            {answer:"Die häufigen Niederschläge verschlechtern die Wasserqualität.", isCorrect:false},
+            {answer:"Man hat große Schwierigkeiten, übr genügend Wasser zu verfügen.", isCorrect:true},
+        ]
+    },{
+        question:"Die Planung sollte noch einmal gründlich überarbeitet werden.",
+        answersList: [
+            {answer:"Wir haben Zweifel an der Organisation des Vorhabens.", isCorrect:true},
+            {answer:"Die Fachleute empfahlen uns unser Projekt zu verabschieden.", isCorrect:false},
+            {answer:"Bei der Überprüfung waren keine Mängel festgestellt worden.", isCorrect:false},
+        ]
+    },{
+        question:"Unsere Verbesserungsvorschläge wurden vom Gemeinderat abgelehnt.",
+        answersList: [
+            {answer:"Die von uns geforderten Maßnahmen wurden mehrheitlich angenommen.", isCorrect:false},
+            {answer:"Was wir verbessern wollten, ist auf Ablehnung gestoßen.", isCorrect:true},
+            {answer:"Die Politiker wollten unsere Vorschläge noch überarbeiten.", isCorrect:false},
+        ]
+    },{
+        question:"Gleich und Gleich gesellt sich gern, so lautet ein Sprichwort.",
+        answersList: [
+            {answer:"Eine gleiche Gesinnung zu haben ist bei der Partnersuche unbedeutend. ", isCorrect:false},
+            {answer:"Wenn Paare den gleichen Humor haben, passen sie selten zusammen.", isCorrect:false},
+            {answer:"Menschen mit gemeinsamen Interessen finden leichter zueinander.", isCorrect:true},
+        ]
+    },{
+        question:"Ein Haustier kann eine wichtige soziale Funktion erfüllen.",
+        answersList: [
+            {answer:"Ja, vor allem können Kinder so Verantwortung für andere erlernen.", isCorrect:true},
+            {answer:"Klar, das Alleinsein wird so für ältere Menschen komplizierter.", isCorrect:false},
+            {answer:"Richtig, auf einen Hund können Sie sich immer verlassen.", isCorrect:false},
+        ]
+    },{
+        question:"Mal sehen, ob ich mit der Dating-App einen Partner finde.",
+        answersList: [
+            {answer:"Technischer Fortschritt bedeutet auch immer Glück in der Liebe.", isCorrect:false},
+            {answer:"Alle Mittel sind recht, dem Glück auf die Sprünge zu helfen.", isCorrect:true},
+            {answer:"Einsamen Herzen kann man nicht mehr helfen.", isCorrect:false},
+        ]
+    },{
+        question:"Wenn Menschen verliebt sind, befinden sie sich in einem rauschähnlichen Zustand.",
+        answersList: [
+            {answer:"Durch Drogenmissbrauch kann man die große Liebe finden.", isCorrect:false},
+            {answer:"Sie durchschauen alles ganz genau und fühlen sich entspannt.", isCorrect:false},
+            {answer:"Es ist, als hätte man zuviel getrunken.", isCorrect:true},
+        ]
+    },{
+        question:"Paula könnte sich bei dem Treffen gelangweilt haben.",
+        answersList: [
+            {answer:"Ja, sie hat gleich gemerkt, was für ein Langweiler Noah ist.", isCorrect:true},
+            {answer:"Sie hat lange dafür gebraucht seine Schwächen zu erkennen.", isCorrect:false},
+            {answer:"Mir schien, dass sie sehr von ihm angetan war.", isCorrect:false},
+        ]
+    },
+    
+
+    {
+        question:"Unser Gast ist nicht zum Essen erschienen.",
+        answersList: [
+            {answer:"Ja, er könnte sich mal wieder verfahren haben.", isCorrect:true},
+            {answer:"Vielleicht hätten wir ihn suchen sollen.", isCorrect:false},
+            {answer:"Möglicherweise hat er nichts zum Essen vorbereitet.", isCorrect:false},
+        ]
+    },{
+        question:"Er will schon mehrmals dort gewesen sein.",
+        answersList: [
+            {answer:"Man sollte es ihm abnehmen.", isCorrect:false},
+            {answer:"Zumindest behauptet er das ständig.", isCorrect:true},
+            {answer:"Darauf kannst du dich verlassen.", isCorrect:false},
+        ]
+    },{
+        question:"Als Kind konnte ich ganz gut Flöte spielen.",
+        answersList: [
+            {answer:"Und auch heute noch bin ich sehr unmusikalisch.", isCorrect:false},
+            {answer:"Das haben meine Eltern auch immer gesagt.", isCorrect:false},
+            {answer:"Heutzutage habe ich keine Zeit mehr für ein Musikinstrument.", isCorrect:true},
+        ]
+    },{
+        question:"Konntet ihr nach dem Unwetter die Reise fortsetzen?",
+        answersList: [
+            {answer:"Ja, aber der Reisebus hatte repariert werden müssen.", isCorrect:true},
+            {answer:"Nein, es ging so weiter, wie zuvor.", isCorrect:false},
+            {answer:"Klar, denn der Bus hätte eine Reparatur dringend nötig gehabt.", isCorrect:false},
+        ]
+    },{
+        question:"Meine Schlüssel sind weg.",
+        answersList: [
+            {answer:"Sie könnten trotzdem leicht verloren gehen.", isCorrect:false},
+            {answer:"Es war nicht einfach, sie zu verlegen.", isCorrect:false},
+            {answer:"Sie könnten verloren gegangen sein.", isCorrect:true},
+        ]
+    },{
+        question:"Ich bin mir ziemlich sicher, dass Sie Recht haben.",
+        answersList: [
+            {answer:"Ich vermute, dass Sie Recht haben.", isCorrect:true},
+            {answer:"Es könnte stimmen, dass Sie Recht haben.", isCorrect:false},
+            {answer:"Möglich, dass Sie Recht haben.", isCorrect:false},
+        ]
+    },{
+        question:"Das Bild dürfte mindestens 50 Jahre alt sein.",
+        answersList: [
+            {answer:"Es ist kaum denkbar, dass es so alt ist.", isCorrect:false},
+            {answer:"Ich bin mir ziemlich sicher, dass es so alt ist.", isCorrect:true},
+            {answer:"Ohne jeden Zweifel ist es so alt.", isCorrect:false},
+        ]
+    },{
+        question:"Wahrscheinlich bekommt sie die Stelle in der Firma.",
+        answersList: [
+            {answer:"Undenkbar, dass sie genommen wird.", isCorrect:false},
+            {answer:"Trotz aller Bedenken wird sie die Stelle bekommen.", isCorrect:false},
+            {answer:"Sie soll die neue Stelle schon bald bekommen.", isCorrect:true},
+        ]
+    },{
+        question:"Wir gehen davon aus, dass die Reparatur nicht lange dauert.",
+        answersList: [
+            {answer:"Wir sind davon überzeugt, dass es sehr schnell geht.", isCorrect:true},
+            {answer:"Wir hoffen, dass wir alles wieder hinkriegen.", isCorrect:false},
+            {answer:"Meistens müssen unsere Kunden mehrere Wochen warten.", isCorrect:false},
+        ]
+    },{
+        question:"Er behauptet immer noch, er habe die Rechnung schon bezahlt.",
+        answersList: [
+            {answer:"Er müsste inzwischen keine Schulden mehr haben.", isCorrect:false},
+            {answer:"Er soll schon alle Rechnungen bezahlt haben.", isCorrect:false},
+            {answer:"Er will schon alles bezahlt haben.", isCorrect:true},
+        ]
+    },
+
+].sort(() => Math.random() - 0.5);
+
+export default QuestionsList;
